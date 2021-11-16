1d7996a9c42c3938a853477b6b14add3a75fc4b6
LorisGuerrin <loris.guerrin@gmail.com>
2021-11-16 12:07:33 +0100

    test V2.2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
