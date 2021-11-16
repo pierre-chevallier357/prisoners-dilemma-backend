52324dfca6c99495e7dec7de6202d854d75bc3bb
LorisGuerrin <loris.guerrin@gmail.com>
2021-11-16 12:19:15 +0100

    Retour il ne faut pas toucher a @Bean

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -66,4 +66,15 @@ public class Main {
     }
   }
 
+  @Bean
+  public DataSource dataSource() throws ServerException {
+    if (dbUrl == null || dbUrl.isEmpty()) {
+      return new HikariDataSource();
+    } else {
+      HikariConfig config = new HikariConfig();
+      config.setJdbcUrl(dbUrl);
+      return new HikariDataSource(config);
+    }
+  }
+
 }
