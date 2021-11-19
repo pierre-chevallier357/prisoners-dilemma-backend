ca1074f1286afb9826116b916d1f96b54011631d
LorisGuerrin <loris.guerrin@gmail.com>
2021-11-19 12:58:13 +0100

    corectif

--- src/main/java/com/example/strategie/Rancunier.java
+++ src/main/java/com/example/strategie/Rancunier.java
@@ -10,10 +10,11 @@ public class Rancunier extends Strategie{
 	public Coup prochainCoup(ArrayList<Coup> historiqueJ1, ArrayList<Coup> historiqueJ2) {
 		Coup coup = null;
 		try{
-			if((historiqueJ2.get(historiqueJ2.size()-1) == Coup.TRAHIR)|| (historiqueJ1.get(historiqueJ1.size()-1) == Coup.TRAHIR)) {
-				coup = Coup.TRAHIR;
+			for(Coup c : historiqueJ2) {
+				if( c == Coup.TRAHIR){
+					coup = Coup.TRAHIR;
+				}
 			}
-			
 		}
 		catch(Exception e){
 			coup = coupAleatoire(0.5);
