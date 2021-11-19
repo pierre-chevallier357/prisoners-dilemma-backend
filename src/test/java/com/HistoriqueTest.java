5a218c1d7c7156a59f9548cecb8f3161563e8db8
LorisGuerrin <loris.guerrin@gmail.com>
2021-11-19 21:16:59 +0100

    fin test historique

--- src/test/java/com/HistoriqueTest.java
+++ src/test/java/com/HistoriqueTest.java
@@ -2,6 +2,8 @@ package com;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 import com.example.joueur.*;
@@ -24,10 +26,42 @@ class HistoriqueTest {
     }
 
     @Test
-    void testCalculRes() {
+    void testCalculPoint() {
         j.setCoup(Coup.COOPERER);
         j.setResultat(Resultat.P);
         hj.addCoupRes(j);
         assertEquals(1, hj.calculPoint());
+    }
+
+    @Test
+    void testGetResultat() {
+        j.setCoup(Coup.COOPERER);
+        j.setResultat(Resultat.P);
+        hj.addCoupRes(j);
+        assertEquals(Resultat.P, hj.getResultat(0));
+    }
+
+    @Test
+    void testGetCoup() {
+        j.setCoup(Coup.COOPERER);
+        j.setResultat(Resultat.P);
+        hj.addCoupRes(j);
+        assertEquals(Coup.COOPERER, hj.getCoup(0));
+    }
+
+    @Test
+    void testGetListeCoup() {
+        
+	    ArrayList<Coup> coupList = new ArrayList<>();
+
+        assertEquals(coupList.getClass().getSimpleName(), hj.getListCoup().getClass().getSimpleName());
+    }
+
+    @Test
+    void testGetListeRes() {
+        
+	    ArrayList<Resultat> resultatList = new ArrayList<>();
+
+        assertEquals(resultatList.getClass().getSimpleName(), hj.getListResultat().getClass().getSimpleName());
     }
 }
