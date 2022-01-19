577211c0b2ae5cebb15fea1eec40576021cf5691
LorisGuerrin <loris.guerrin@gmail.com>
2022-01-19 13:37:45 +0100

    tools strategie

--- src/test/java/com/StrategieTest.java
+++ src/test/java/com/StrategieTest.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test;
 
 import com.example.joueur.*;
 import com.example.strategie.*;
+import com.example.Tools;
 
 class StrategieTest {
     @Test
     void testStrategie() {
 	    Coup coup  = Coup.COOPERER;
 
-        assertEquals(coup.getClass().getSimpleName(), Strategie.coupAleatoire(0.5).getClass().getSimpleName());
+        assertEquals(coup.getClass().getSimpleName(), Tools.coupAleatoire(0.5).getClass().getSimpleName());
     }
 }
